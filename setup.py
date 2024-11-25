6f164f9682197174a50beadd48bb805ee8e06a69
Emily Hu <emilyhu21030@gmail.com>
2024-11-24 21:26:07 -0800

    set up code base, added Canadarm1/2 .urdfs

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from glob import glob
+
+package_name = 'code'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/rviz', glob('rviz/*')),
+        ('share/' + package_name + '/urdf', glob('urdf/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='robot',
+    maintainer_email='robot@todo.todo',
+    description='The 133a HW3 Code',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'initial_movement = code.initial_movement:main',
+        ],
+    },
+)
